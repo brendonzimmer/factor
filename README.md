0aff47878e4951dfeefef63d6e0e2ad787692738
Brendon Zimmer <brendonzimmerbusiness@gmail.com>
2023-05-18 08:06:11 -0700

    Completely refactor everything! Much faster factorizing, improved error handling and cli args

---
+++ README.md
@@ -0,0 +1,9 @@
+# Factor!
+
+Good to know:
+
+- https://en.wikipedia.org/wiki/Integer_factorization
+
+- https://en.wikipedia.org/wiki/Baillie%E2%80%93PSW_primality_test
+
+- https://www.alpertron.com.ar/ECM.HTM
--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,18 @@
-use std::{env::args, process::exit, num::IntErrorKind, time::{Duration, Instant}};
+mod args;
+mod factor;
 
-// Print factors and time elapsed
-fn print_results(factors: &Vec<u64>, duration: Option<Duration>) {
-    if let Some(time) = duration { if time.as_secs() > 0 { print!("[{}.{}s] ", time.as_secs(), time.subsec_millis()/10); } }
-    else { print!("Found: "); };
-
-    if factors.len() > 0 { for factor in factors { print!("{} ", factor); } }
-    else if let Some(_) = duration { print!("Prime: 1"); }
-    else { print!("None"); };
-    println!();
-}
-
-// Find factors and duration
-fn factor_arg(mut arg: u64) -> (Vec<u64>, Duration) {
-    let mut factors: Vec<u64> = vec![];
-
-    let now = Instant::now();
-    for i in 2..=(arg as f64).sqrt() as u64 {       
-        if now.elapsed().as_secs() >= 100 { println!("Timeout: Running for 100s"); print_results(&factors, None); exit(3) }
-        if i > arg { break };
-        while arg % i == 0 { arg /= i; factors.push(i); };
-    };
-
-    (factors,  now.elapsed())
-}
-
-// Parse input to number
-fn parse_arg(mut arg: String) -> u64 {
-    arg = if arg.contains('-') { arg.replace('-', "") } else { arg };
-    arg = if arg.contains(',') { arg.replace(',', "") } else { arg };
-    arg = if arg.contains('.') { arg.replace('.', "") } else { arg };
-    arg = if arg.contains('_') { arg.replace('_', "") } else { arg };
-
-    match arg.parse() {
-        Ok(num) => num,
-        Err(err) => {
-            match err.kind() {
-                IntErrorKind::InvalidDigit => { eprintln!("Error: Only use (0-9), comma, underscore, or period"); exit(2); },
-                IntErrorKind::PosOverflow => { eprintln!("Error: Integer too large\nMaxInt: {}", u64::MAX); exit(2); }
-                IntErrorKind::Empty => { eprintln!("Error: Must include numbers (0-9)"); exit(2); }
-                _ => { eprintln!("Error: {}", err); exit(2); }
-            }
-        }
-    }
-}
-
-// Get input as string
-fn get_input() -> String {
-    match args().nth(1) {
-        Some(arg) => arg,
-        None => { println!("Usage: COMMAND <number>"); exit(1); }
-    }
-}
+use args::parse;
+use factor::{trial_division, Params};
 
 // Factor the given number
 fn main() {
-    let arg = get_input();
-    // println!("String: {}", arg_str);
+    let args: Params = match parse() {
+        Ok(args) => args.into(),
+        Err(err) => return println!("{}", err),
+    };
 
-    let arg = parse_arg(arg);
-    // println!("u64: {}", arg);
-
-    let (factors, duration) = factor_arg(arg);
-    // println!("{:?}", factors);
-
-    print_results(&factors, Some(duration));
+    match trial_division(args) {
+        Ok(factors) => println!("{}", factors),
+        Err(err) => println!("{}", err),
+    }
 }
